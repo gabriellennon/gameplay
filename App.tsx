982d5acdb80a42cc4d31bcccf34d0117b590fe62
Gabriel Lennon <gabriellennon@Gabriels-MacBook-Air.local>
2021-06-21 20:35:21 -0300

    uso de fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+//hoock para carregar fontes
+import { useFonts } from 'expo-font';
+//importando as fontes
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
+
 import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  })
+
+  //enquanto as fontes nao carregarem, deixa e tela de splash
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
   return (
     <SignIn />
   );
